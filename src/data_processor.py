b9876c848bdad070d7c93f730f29db3a0b503318
thebigduck <eddteller@gmail.com>
2024-11-23 21:29:17 -0500

    cleanup

--- src/data_processor.py
+++
@@ -1,9 +0,0 @@
-# src/data_processor.py
-
-def preprocess_data(data):
-    """
-    Cleans and preprocesses the raw stock data.
-    """
-    data = data.dropna()
-    data['returns'] = data['adj close'].pct_change()  # Use 'adj close' for returns
-    return data
